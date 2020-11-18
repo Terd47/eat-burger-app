7366a7e742c2426904000278a63c02816336f523
Terd47 <terdlar@gmail.com>
2020-11-18 17:14:41 -0500

    updated schema and seeds files to fix name error

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
 INSERT INTO burgers (burger_name) VALUES ('Quater Pounder');
 INSERT INTO burgers (burger_name) VALUES ('Ham Burger');
-INSERT INTO burgers (name, sleepy) VALUES ('Big Mac', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Fish Fil A', true);
 INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
