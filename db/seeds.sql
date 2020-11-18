654981c98d3c44f4a79671c1ecbfc3d598398eed
Terd47 <terdlar@gmail.com>
2020-11-18 17:00:45 -0500

    created database schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Quater Pounder');
+INSERT INTO burgers (burger_name) VALUES ('Ham Burger');
+INSERT INTO burgers (name, sleepy) VALUES ('Big Mac', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Fish Fil A', true);
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
