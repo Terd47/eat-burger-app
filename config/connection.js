662ccd994259ba9d43e0e58ff72d715f3b528fc8
Terd47 <terdlar@gmail.com>
2020-11-25 17:39:11 -0500

    fixed heroku appp error about connection denied to database

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,8 @@
 // Set up MySQL connection.
 let mysql = require("mysql");
 
-// let connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "School1",
-//   database: "burgers_db"
-// });
+let connection;
+
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else {
